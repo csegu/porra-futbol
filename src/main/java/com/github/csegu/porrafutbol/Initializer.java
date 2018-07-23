c2ea29506f682bfc6c4cb48392e3bf45b071c449
dperezcabrera <dperezcabrera@gmail.com>
2018-07-23 18:41:47 +0200

    logger errors fixed at Initializer and other improvements
    - Refactored pom.xml
    - Added constructor with @Autowired at Initializer.
    - Removed unused imports.
    - Added javax.persistence annotations to Match

--- src/main/java/com/github/csegu/porrafutbol/Initializer.java
+++ src/main/java/com/github/csegu/porrafutbol/Initializer.java
@@ -2,32 +2,26 @@ package com.github.csegu.porrafutbol;
 
 import com.github.csegu.porrafutbol.model.Team;
 import com.github.csegu.porrafutbol.repository.TeamRepository;
-import java.util.Arrays;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
-public class Initializer implements CommandLineRunner{
+@AllArgsConstructor(onConstructor = @__({@Autowired}))
+public class Initializer implements CommandLineRunner {
 
-    @Autowired
     private TeamRepository TeamRepository;
-    
-    Logger logger = LoggerFactory.getLogger("Inicio de la aplicacion");
-
 
     @Override
-    public void run(String...args) throws Exception {
-        logger.info("Application started with command-line arguments: {} . \n To kill this application, press Ctrl + C.", Arrays.toString(args));
-                
+    public void run(String... args) throws Exception {
+        log.info("Application started with command-line arguments: {} . \n To kill this application, press Ctrl + C.", (Object)args);
         TeamRepository.save(new Team(1L, "Real Madrid C.F."));
         TeamRepository.save(new Team(2L, "F.C. Barcelona"));
         TeamRepository.save(new Team(3L, "Bayern Munchen"));
         TeamRepository.save(new Team(4L, "Juventus"));
-        logger.trace("Carga inciial de equipos.");
-        
+        log.trace("Carga inicial de equipos.");
     }
-
 }
--- src/main/java/com/github/csegu/porrafutbol/repository/MatchRepository.java
+++ src/main/java/com/github/csegu/porrafutbol/repository/MatchRepository.java
@@ -1,12 +1,8 @@
 package com.github.csegu.porrafutbol.repository;
 
 import com.github.csegu.porrafutbol.model.Match;
-import java.util.Optional;
-import java.util.List;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Long>  {
